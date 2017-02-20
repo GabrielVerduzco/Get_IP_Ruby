266ce9d0e96c532937694fead12d9a9af8176398
OmauryMarmolejo <vmarmolejo@ucol.mx>
2017-02-20 11:49:53 -0600

    changes

--- api.rb
+++ api.rb
@@ -7,8 +7,8 @@ require 'json'
 def open(url)
   Net::HTTP.get(URI.parse(url))
 end
+puts "Escribe una url"
+url = gets.chomp
 
-
-
-page_content = open('http://freegeoip.net/json/github.com')
+page_content = open('http://freegeoip.net/json/' + url)
 puts page_content
