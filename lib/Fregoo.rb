29f31018c91dac4e4a409937a0efc10c169cb9c8
GabrielVerduzco <verduzcogab@gmail.com>
2017-02-21 13:08:42 -0600

    Module

--- lib/Fregoo.rb
+++ lib/Fregoo.rb
@@ -4,35 +4,37 @@ require 'json'
 require_relative 'DataIp'
 
 
-
-class Request
-
-
-  attr_accessor :hash
+module Fregoo
 
   def self.get(remote)
-    fregoo = new(remote)
+    fregoo = Request.new(remote)
     RequestObject.new(fregoo.hash)
   end
 
-  def initialize(remote)
-    go_url(remote)
-  end
-  private
-  def go_url(url)
-    open('http://freegeoip.net/json/' + url)
-  end
 
-  def open(url)
-    url = Net::HTTP.get(URI.parse(url))
-    parse_url url
-  end
+    class Request
+      attr_accessor :hash
 
-  def parse_url(url)
-    @hash = JSON.parse url
-  end
 
-end
+      def initialize(remote)
+        go_url(remote)
+      end
+      private
+      def go_url(url)
+        open('http://freegeoip.net/json/' + url)
+      end
+
+      def open(url)
+        url = Net::HTTP.get(URI.parse(url))
+        parse_url url
+      end
+
+      def parse_url(url)
+        @hash = JSON.parse url
+      end
+    end
+
+  end
 
 
 
